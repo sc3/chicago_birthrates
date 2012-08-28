ecbe105d79a6f87bbd74b077043d55fb45bb06fa
David Eads <davideads@gmail.com>
2012-08-28 00:14:43 -0500

    add bones of birthrate data reader

--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
+import requests
+import csv
+import StringIO
 app = Flask(__name__)
+
+SOCRATA_DATA_URL = 'https://data.cityofchicago.org/api/views/4arr-givg/rows.csv?accessType=DOWNLOAD'
 
 @app.route('/')
 def index():
+    response = requests.get(SOCRATA_DATA_URL) # Get data
+    response_string = StringIO.StringIO(response.text) # Magic Python code to convert text
+    for row in csv.DictReader(response_string): # row is a dict like: {'Community Area Name': 'EDGEWATER', 'Births 2005': '709'}
+        print row
     return render_template('index.html')
 
 if __name__ == '__main__':
