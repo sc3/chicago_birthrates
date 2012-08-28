f77079818edb4321cf44573d036b2e649e6a203c
David Eads <davideads@gmail.com>
2012-08-28 00:20:24 -0500

    debugging step, all seems to be working well

--- app/app.py
+++ app/app.py
@@ -10,9 +10,15 @@ SOCRATA_DATA_URL = 'https://data.cityofchicago.org/api/views/4arr-givg/rows.csv?
 def index():
     response = requests.get(SOCRATA_DATA_URL) # Get data
     response_string = StringIO.StringIO(response.text) # Magic Python code to convert text
+    data = []
     for row in csv.DictReader(response_string): # row is a dict like: {'Community Area Name': 'EDGEWATER', 'Births 2005': '709'}
-        print row
-    return render_template('index.html')
+        data_row = [ row['Community Area Name'], ]
+        for year in range(1999, 2009):
+            key = 'Births %s' % year
+            data_row.append( row[key] )
+            data.append(data_row)
+    import ipdb; ipdb.set_trace()
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.debug=True
