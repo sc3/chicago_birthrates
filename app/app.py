8559794e181a757bc2fa70b43085720fd2b03993
David Eads <davideads@gmail.com>
2012-08-28 22:03:59 -0500

    add data file to repo, tweak output structure

--- app/app.py
+++ app/app.py
@@ -2,23 +2,31 @@ from flask import Flask, render_template
 import requests
 import csv
 import StringIO
+import os
+
 app = Flask(__name__)
 
 SOCRATA_DATA_URL = 'https://data.cityofchicago.org/api/views/4arr-givg/rows.csv?accessType=DOWNLOAD'
 
 @app.route('/')
 def index():
-    response = requests.get(SOCRATA_DATA_URL) # Get data
-    response_string = StringIO.StringIO(response.text) # Magic Python code to convert text
-    data = []
-    for row in csv.DictReader(response_string): # row is a dict like: {'Community Area Name': 'EDGEWATER', 'Births 2005': '709'}
-        data_row = [ row['Community Area Name'], ]
-        for year in range(1999, 2009):
-            key = 'Births %s' % year
-            data_row.append( row[key] )
-            data.append(data_row)
-    import ipdb; ipdb.set_trace()
-    return render_template('index.html', data=data)
+
+    data = open(os.path.join(os.path.dirname(__file__), 'data', 'chicago-birthrates-1999-2009.csv'), 'r')
+    
+    # Uncomment the next 2 lines to use live data
+    # response = requests.get(SOCRATA_DATA_URL) # Get data
+    # data = StringIO.StringIO(response.text) # Magic Python code to convert text
+
+    # Build up data structure like ('Edgewater', (2008, 500), (2009, 450), ... )
+    table = []
+    for row in csv.DictReader(data): # row is a dict like: {'Community Area Name': 'EDGEWATER', 'Births 2005': '709'}
+        table_row = [ row['Community Area Name'], ]
+        for year in range(1999, 2010):
+            key = 'Birth Rate %s' % year # Construct key name
+            table_row.append( row[key] )
+        table.append(table_row)
+    #import ipdb; ipdb.set_trace()
+    return render_template('index.html', table=table)
 
 if __name__ == '__main__':
     app.debug=True
