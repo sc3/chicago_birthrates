7d1b5b8f059443e138af9d12bca03dcc7e565931
Sabine Ye <wasabine@gmail.com>
2012-08-29 20:25:36 -0500

    added comments and fixed table on html

--- app.py
+++ app.py
@@ -5,31 +5,37 @@ import StringIO
 import os
 import math
 
-app = Flask(__name__)
+app = Flask(__name__) #what flask need to fire up an application (instantiating flask object and name it 'app')
 
 SOCRATA_DATA_URL = 'https://data.cityofchicago.org/api/views/4arr-givg/rows.csv?accessType=DOWNLOAD'
+#convention to use all uppercase to represent a constant
 
-@app.route('/')
+@app.route('/') #whatever the functionm it will be available at '/'
 def index():
 
     data = open(os.path.join(os.path.dirname(__file__), 'data', 'chicago-birthrates-1999-2009.csv'), 'r')
+    #access a file in the data directory called 'chicago-birthrates-1999-2009.csv' ('r' is for read only)
     
     # Uncomment the next 2 lines to use live data
     # response = requests.get(SOCRATA_DATA_URL) # Get data
     # data = StringIO.StringIO(response.text) # Magic Python code to convert text
 
     # Build up data structure like ('Edgewater', (2008, 500), (2009, 450), ... )
-    table = []
     context = {
         'max': 0,
         'min': 100,
         'table': [],
     }
+    """
+    Will create a loop  over 'chicago-birthrates-1999-2009.csv', take the data, extract the part we need and put it in a table. 
+    DictReader will take the first row as a header and other rows will be part of the dictionary.
+
+    """
     for row in csv.DictReader(data): # row is a dict like: {'Community Area Name': 'EDGEWATER', 'Births 2005': '709'}
-        table_row = [ row['Community Area Name'], ]
+        table_row = [ row['Community Area Name'], ] #create a list with first position 'Community area'
         for year in range(1999, 2010):
             key = 'Birth Rate %s' % year # Construct key name
-            value = float(row[key])
+            value = float(row[key]) #convert the key value to a number
             table_row.append( (year, value) )
             if value > context['max']: 
                 context['max'] = value
